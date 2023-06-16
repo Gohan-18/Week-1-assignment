78acb842bd234245edcab50f0c8ce0a765959fd4
Prabhat <pkjha187@gmail.com>
2023-06-16 22:49:35 +0530

    Calculator problrm

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -17,6 +17,82 @@
   - `npm run test-calculator`
 */
 
-class Calculator {}
+class Calculator {
+  constructor() {
+    this.result = 0;
+  }
+
+  add(number) {
+    this.validateNumber(number);
+    this.result += number;
+  }
+
+  subtract(number) {
+    this.validateNumber(number);
+    this.result -= number;
+  }
+
+  multiply(number) {
+    this.validateNumber(number);
+    this.result *= number;
+  }
+
+  divide(number) {
+    this.validateNumber(number);
+    if (number === 0) {
+      throw new Error('Cannot divide by zero');
+    }
+    this.result /= number;
+  }
+
+  clear() {
+    this.result = 0;
+  }
+
+  getResult() {
+    return this.result;
+  }
+
+  calculate(expression) {
+    this.validateExpression(expression);
+    this.clear();
+
+    // Remove all whitespace characters from the expression
+    expression = expression.replace(/\s/g, '');
+
+    // Evaluate the expression using the 'eval' function
+    this.result = eval(expression);
+  }
+
+  validateNumber(number) {
+    if (typeof number !== 'number' || isNaN(number)) {
+      throw new Error('Invalid number');
+    }
+  }
+
+  validateExpression(expression) {
+    if (typeof expression !== 'string') {
+      throw new Error('Expression must be a string');
+    }
+
+    // Check for invalid non-numerical characters
+    if (!/^[\d+\-*/().\s]+$/.test(expression)) {
+      throw new Error('Invalid expression');
+    }
+  }
+}
+
+const calculator = new Calculator();
+
+calculator.add(10);
+calculator.subtract(5);
+calculator.multiply(2);
+calculator.divide(3);
+
+console.log(calculator.getResult()); 
+
+calculator.calculate('10 +   2 *    (   6 - (4 + 1) / 2) + 7');
+console.log(calculator.getResult()); 
+
 
 module.exports = Calculator;
